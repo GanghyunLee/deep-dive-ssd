4cbfd5c36449c1de66f3e4546a775c52dc982729
ghyun.lee <ghyun.lee@samsung.com>
2025-05-16 15:14:24 +0900

    main 복구 (Debug 모드일 때 Test Code 수행)

--- DeepDiveIntoSSD/testshell/main.cpp
+++ DeepDiveIntoSSD/testshell/main.cpp
@@ -3,6 +3,11 @@
 
 int main(void)
 {
+#ifdef _DEBUG
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+#elif
 	IoC ioc{};
 	return (true == TestShellApplication{ ioc, std::cin, std::cout }.Run());
+#endif
 }
