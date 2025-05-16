c3d90e26fc0b4edc90b2f59948fddcc764e5b6da
gyeongmin kwon <rudalsd@naver.com>
2025-05-16 14:21:56 +0900

    ArgManager class 파일 세팅

---
+++ DeepDiveIntoSSD/ssd/ArgManager.cpp
@@ -0,0 +1,17 @@
+#include "ArgManager.h"
+
+std::vector<std::string> ArgManager::commandSplit(int argc, char* argv[]) {
+	std::vector<std::string> args;
+
+	return args;
+}
+
+bool ArgManager::isValid(std::vector<std::string>) {
+	return true;
+}
+
+Arg ArgManager::makeStruct(std::vector<std::string>) {
+	Arg arg;
+
+	return arg;
+}
---
+++ DeepDiveIntoSSD/ssd/ArgManager_test.cpp
@@ -0,0 +1,14 @@
+#include "ArgManager.h"
+#include "gmock/gmock.h"
+
+TEST(ArgManager, commandSplit1) {
+	
+}
+
+TEST(ArgManager, commandSplit2) {
+
+}
+
+TEST(ArgManager, commandSplit3) {
+
+}
