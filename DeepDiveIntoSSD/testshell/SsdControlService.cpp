0eff98507461425bde90c927e6a73e3d66aab765
jklee <dlworb7@gmail.com>
2025-05-16 15:37:01 +0900

    Add TestScenario_1~3

--- DeepDiveIntoSSD/testshell/SsdControlService.cpp
+++ DeepDiveIntoSSD/testshell/SsdControlService.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <iomanip>
 #include "SsdReadResult.h"
 
 using namespace std;
@@ -24,7 +25,7 @@ public:
 	void Write(int lba, unsigned int value) {
 		stringstream ss;
 
-		ss << "ssd.exe W " << to_string(lba) << " " << to_string(value) << endl;
+		ss << "ssd.exe W " << to_string(lba) << " " << "0x" << hex << setw(8) << setfill('0') << value << endl;
 		system(ss.str().c_str());
 	}
 	void Exit() {
@@ -64,5 +65,78 @@ public:
 		cout << "read : ssd.exe R address" << endl;
 		cout << "ssd.exe fullwrite : ssd.exe fullwrite 0xABCDFFFF" << endl;
 		cout << "ssd.exe fullread : ssd.exe fullread 0xABCDFFFF" << endl;
+	}
+};
+
+class SsdControlServiceTestScenario {
+public:
+	bool TestScenario_1() {
+		SsdControlService service;
+		SsdReadResult result;
+		unsigned int value = 1;
+		int i, j, k;
+
+		for (k = 0; k < 20; ++k) {
+			for (i = 0; i < 5; ++i) {
+				service.Write((k * 5) + i, value);
+			}
+			for (j = 0; j < 5; ++j) {
+				result = service.Read((k * 5) + j);
+				if (value != result.data) {
+					return false;
+				}
+			}
+			value += 1;
+		}
+		return true;
+	}
+	bool TestScenario_2() {
+		SsdControlService service;
+		SsdReadResult result;
+		unsigned int value = 1;
+
+		for (int i = 0; i < 30; ++i) {
+			service.Write(4, value);
+			service.Write(0, value);
+			service.Write(3, value);
+			service.Write(1, value);
+			service.Write(2, value);
+
+			result = service.Read(4);
+			if (result.data != service.Read(0).data) {
+				return false;
+			}
+			if (result.data != service.Read(3).data) {
+				return false;
+			}
+			if (result.data != service.Read(1).data) {
+				return false;
+			}
+			if (result.data != service.Read(2).data) {
+				return false;
+			}
+		}
+		return true;
+	}
+	bool TestScenario_3() {
+		SsdControlService service;
+		SsdReadResult result;
+		unsigned int value = 1;
+
+		for (int i = 0; i < 200; ++i) {
+			std::srand(std::time(0));
+			unsigned int value = std::rand();
+
+			service.Write(0, value);
+			service.Write(99, value);
+
+			if (value != service.Read(0).data) {
+				return false;
+			}
+			if (value != service.Read(99).data) {
+				return false;
+			}
+		}
+		return true;
 	}
 };
