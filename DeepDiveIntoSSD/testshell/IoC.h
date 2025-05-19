4d3f9dca0d3a3bb715caf27256e20d6238128325
ghyun.lee <ghyun.lee@samsung.com>
2025-05-19 09:00:42 +0900

    [refactoring] ExitCommand 제거
    굳이 클래스를 뺄 필요 없이 string으로만 비교하도록 작성하는 것이 효율적이라고 판단

--- DeepDiveIntoSSD/testshell/IoC.h
+++ DeepDiveIntoSSD/testshell/IoC.h
@@ -1,33 +1,11 @@
 #pragma once
 #include <vector>
 
-#include "ExitCommandMapper.h"
-
 class ICommandMapper;
-
-
 
 class IoC
 {
 public:
-	IoC()
-	{
-		_exitCommandMapper = new ExitCommandMapper();
-	}
-
-	~IoC()
-	{
-		if (_exitCommandMapper == nullptr)
-		{
-			delete _exitCommandMapper;
-			_exitCommandMapper = nullptr;
-		}
-	}
-
-public:
 	std::vector<ICommandMapper*> GetCommandMappers();
-
-protected:
-	ICommandMapper* _exitCommandMapper = nullptr;
 };
 
