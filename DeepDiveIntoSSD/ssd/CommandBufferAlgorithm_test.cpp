b63cc40835cf2d8febc6a15bfc4d748b128b1cd0
m00nstarr <moonstar11430@gmail.com>
2025-05-20 13:53:18 +0900

    [Feat] mergeAble 테스트 추가

--- DeepDiveIntoSSD/ssd/CommandBufferAlgorithm_test.cpp
+++ DeepDiveIntoSSD/ssd/CommandBufferAlgorithm_test.cpp
@@ -29,16 +29,16 @@ TEST(CommandBufferAlgorithm, ignoreCommand3) {
 }
 
 TEST(CommandBufferAlgorithm, ignoreCommand4) {
-	std::vector<Arg> buffer = { {2,10,"0xABCD1234"}, {2,11,"0xABCD1234"}, {2,12,"0xABCD1234"}, {2,13,"0xABCD1234"}, {3,10,"5"}};
+	std::vector<Arg> buffer = { {2,10,"0xABCD1234"}, {2,11,"0xABCD1234"}, {2,12,"0xABCD1234"}, {2,13,"0xABCD1234"}, {3,10,"5"} };
 	CommandBufferAlgorithm cba;
-	std::vector<Arg> expected = { {3,10,"5"}, {0,}, {0,}, {0,}, {0,}};
+	std::vector<Arg> expected = { {3,10,"5"}, {0,}, {0,}, {0,}, {0,} };
 	std::vector<Arg> ret = cba.ignoreCommand(buffer);
 
 	EXPECT_EQ(expected, ret);
 }
 
 TEST(CommandBufferAlgorithm, ignoreCommand5) {
-	std::vector<Arg> buffer = { {2,10,"0xABCD1234"}, {3,10,"5"}, {2,11,"0xABCD1234"}, {3,10,"5"}, {2,13,"0xABCD1234"}};
+	std::vector<Arg> buffer = { {2,10,"0xABCD1234"}, {3,10,"5"}, {2,11,"0xABCD1234"}, {3,10,"5"}, {2,13,"0xABCD1234"} };
 	CommandBufferAlgorithm cba;
 	std::vector<Arg> expected = { {3,10,"5"}, {2,13,"0xABCD1234"}, {0,}, {0,}, {0,} };
 	std::vector<Arg> ret = cba.ignoreCommand(buffer);
@@ -80,4 +80,33 @@ TEST(CommandBufferAlgorithm, fastRead) {
 	Arg ret = cba.fastRead(buffer);
 
 	EXPECT_EQ(expected, ret);
+}
+
+TEST(CommandBufferAlgorithm, CanMergeTest1) {
+	CommandBufferAlgorithm cba;
+	Arg a = { COMMAND_TYPE::ERASE, 10, "3" };
+	Arg b = { COMMAND_TYPE::ERASE, 12, "3" };
+
+	cba.mergeAble(a, b);
+	EXPECT_TRUE(cba.mergeAble(a, b));
+}
+
+TEST(CommandBufferAlgorithm, CanMergeTest2) {
+	CommandBufferAlgorithm cba;
+	EXPECT_TRUE(cba.mergeAble({ COMMAND_TYPE::ERASE, 10, "4" }, { COMMAND_TYPE::ERASE, 10, "4" }));
+}
+
+TEST(CommandBufferAlgorithm, CanMergeTest3) {
+	CommandBufferAlgorithm cba;
+	EXPECT_TRUE(cba.mergeAble({ COMMAND_TYPE::ERASE, 10, "2" }, { COMMAND_TYPE::ERASE, 12, "2" }));
+}
+
+TEST(CommandBufferAlgorithm, CantMergeTest1) {
+	CommandBufferAlgorithm cba;
+	EXPECT_FALSE(cba.mergeAble({ COMMAND_TYPE::ERASE, 10, "1" }, { COMMAND_TYPE::ERASE, 12, "1" }));
+}
+
+TEST(CommandBufferAlgorithm, CantMergeTest2) {
+	CommandBufferAlgorithm cba;
+	EXPECT_FALSE(cba.mergeAble({ COMMAND_TYPE::ERASE, 10, "3" }, { COMMAND_TYPE::ERASE, 10, "3" }));
 }
